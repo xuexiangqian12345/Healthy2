fdfb5c259a91f4f547e02ba1bbb7bf1b86401cd4
xxq <xuexiangqian12345@163.comm>
2019-12-29 11:55:07 +0800

    补充上传可用文件

--- app/src/main/java/com/example/windows/healthy/MainActivity.java
+++ app/src/main/java/com/example/windows/healthy/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.windows.healthy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
     }
 }
